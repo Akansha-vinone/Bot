85e42f0beb6bab0ee3d3474ab5351e344bb1969c
Akansha-vinone <akansha@mail.vinove.com>
2025-01-31 11:34:27 +0530

    First changes

--- App/settings.py
+++ App/settings.py
@@ -12,10 +12,16 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+env_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env', '.env')
+
+
+load_dotenv(dotenv_path=env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -133,5 +139,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Use your email provider
 EMAIL_PORT = 587  # Common port for SMTP
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'cute.jaati17@gmail.com'
-EMAIL_HOST_PASSWORD = 'pgyl swdirrzi wsoe'
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD ')
